ef9df43aa34137083fce1e803b2640bc31a5c9f7
Zhuoranll <li.zhuoran@northeastern.edu>
2024-04-06 00:34:07 -0700

    delete wrong import

--- bid/views.py
+++ bid/views.py
@@ -17,7 +17,7 @@ from django.views.decorators.http import require_http_methods
 from .forms import BidForm
 from rest_framework import generics
 from .models import Property
-from .serializers import PropertySerializer, BidSerializer
+from .serializers import PropertySerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
