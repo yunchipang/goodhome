af0eda4bd3b468239ca87464abc4999a591d2d18
NaomiW7 <wang.haona@northeastern.edu>
2024-04-10 11:20:31 -0700

    implement logout and profile feature

--- core/settings.py
+++ core/settings.py
@@ -82,7 +82,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=360),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
